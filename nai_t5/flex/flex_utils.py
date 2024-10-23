18d0f2ebee77e6e64566a16615ffbd4ff664c564
veryyoldman <6755083+veryyoldman@users.noreply.github.com>
2024-10-23 21:05:44 +0000

    update with encoder flex attention

--- nai_t5/flex/flex_utils.py
+++ nai_t5/flex/flex_utils.py
@@ -1,8 +1,14 @@
-from typing import Optional, Protocol
-import math
+from typing import Optional, Protocol, TYPE_CHECKING, Any
 from contextlib import nullcontext
-from torch import FloatTensor, IntTensor
+from functools import lru_cache
+from torch import FloatTensor, IntTensor, BoolTensor
 import torch
+
+if TYPE_CHECKING:
+    # avoid runtime import to prevent explosion on older PyTorch
+    from torch.nn.attention.flex_attention import BlockMask
+else:
+    BlockMask = _mask_mod_signature = Any
 
 # there's an official type _score_mod_signature for this but our protocol provides more detail
 class ScoreMod(Protocol):
@@ -19,15 +25,18 @@ class ScoreMod(Protocol):
         kv_idx: IntTensor,
     ) -> FloatTensor: ...
 
-# TODO PyTorch 2.5: prefer builtin
-# from torch.nn.attention.flex_attention import _vmap_for_bhqkv
-from .flex_attn_backport import _vmap_for_bhqkv
-
-# TODO This was moved on nightly, this enables 2.5 and 2.6 | we should remove this once 2.5 is no longer supported
-try:
-    from torch._dynamo._trace_wrapped_higher_order_op import TransformGetItemToIndex
-except ImportError:
-    from torch._higher_order_ops.flex_attention import TransformGetItemToIndex
+class MaskMod(Protocol):
+    """
+    Same purpose as torch.nn.attention.flex_attention._mask_mod_signature
+    but documents parameter names and datatypes.
+    """
+    @staticmethod
+    def __call__(
+        batch: IntTensor,
+        head: IntTensor,
+        q_idx: IntTensor,
+        kv_idx: IntTensor,
+    ) -> BoolTensor: ...
 
 # based on attention-gym, BSD-3-Clause License
 # https://github.com/pytorch-labs/attention-gym/blob/f7c93ded4abf9fd8d7dc9d8bcbf57e420b891e2d/attn_gym/utils.py#L23
@@ -42,6 +51,13 @@ def create_score_mod(
     batch_idx: int = 0,
     head_idx: int = 0,
 ) -> FloatTensor:
+    from torch.nn.attention.flex_attention import _vmap_for_bhqkv
+    # TODO This was moved on nightly, this enables 2.5 and 2.6 | we should remove this once 2.5 is no longer supported
+    try:
+        from torch._dynamo._trace_wrapped_higher_order_op import TransformGetItemToIndex
+    except ImportError:
+        from torch._higher_order_ops.flex_attention import TransformGetItemToIndex
+
     B = 1
     H = 1
     M = query.shape[0]
@@ -73,6 +89,13 @@ def create_bias(
     device: str = "cuda",
     _compile: bool = False,
 ) -> FloatTensor:
+    from torch.nn.attention.flex_attention import _vmap_for_bhqkv
+    # TODO This was moved on nightly, this enables 2.5 and 2.6 | we should remove this once 2.5 is no longer supported
+    try:
+        from torch._dynamo._trace_wrapped_higher_order_op import TransformGetItemToIndex
+    except ImportError:
+        from torch._higher_order_ops.flex_attention import TransformGetItemToIndex
+
     b = torch.arange(0, batch, device=device)
     h = torch.arange(0, heads, device=device)
     m = torch.arange(0, q_len, device=device)
@@ -86,3 +109,28 @@ def create_bias(
         out = mod(scores, b, h, m, n)
 
     return out
+
+# in practice we are no longer using this; tried using it inside T5EncoderSelfAttention#forward…
+# it compiled the flex_attention operation successfully, but it caused failures when compiling
+# T5EncoderSelfAttention#forward itself (double/nested compilation problems I suppose?).
+@lru_cache(maxsize=1)
+def get_compiled_flex():
+    """
+    Memoize the compilation of flex_attention.
+    """
+    from torch.nn.attention.flex_attention import flex_attention
+    return torch.compile(flex_attention, dynamic=False)
+
+# caches last-used-mask only (an unbounded cache could leak memory)
+@lru_cache(maxsize=1)
+def create_block_mask_cached(
+    mask_mod: MaskMod,
+    batch: int,
+    heads: int,
+    q_len: int,
+    kv_len: int,
+    device="cuda",
+) -> BlockMask:
+    from torch.nn.attention.flex_attention import create_block_mask
+    block_mask = create_block_mask(mask_mod, batch, heads, q_len, kv_len, device=device)
+    return block_mask
--- nai_t5/t5_encoder.py
+++ nai_t5/t5_encoder.py
@@ -1,5 +1,5 @@
 import math
-from typing import Optional, Protocol
+from typing import Optional, Protocol, TYPE_CHECKING, NamedTuple, Any
 from itertools import chain
 from functools import partial
 
@@ -20,7 +20,13 @@ from .t5_common import (
     flash_attention_flops,
     get_ffn_factory,
 )
-from .flex.flex_utils import ScoreMod
+from .flex.flex_utils import ScoreMod, MaskMod
+
+if TYPE_CHECKING:
+    # avoid runtime import to prevent explosion on older PyTorch
+    from torch.nn.attention.flex_attention import BlockMask
+else:
+    BlockMask = _mask_mod_signature = Any
 
 ####
 #### T5 encoder self-attention
@@ -88,9 +94,8 @@ class T5EncoderSelfAttention(nn.Module):
         nn.init.normal_(self.qkv_proj.weight, std=1 / math.sqrt(self.config.hidden_dim))
         nn.init.normal_(self.o_proj.weight, std=1 / math.sqrt(self.config.hidden_dim * self.config.num_layers))
 
-# TODO:
 class T5EncoderSelfAttentionFlex(nn.Module):
-    score_mod: ScoreMod
+    score_mod: Optional[ScoreMod] = None
     qkv_proj: Linear
     o_proj: Linear
     head_dim: int
@@ -98,18 +103,16 @@ class T5EncoderSelfAttentionFlex(nn.Module):
     dropout: float
     config: T5Config
 
-    def __init__(self, config: T5Config, emb_weight: FloatTensor) -> None:
+    def __init__(self, config: T5Config) -> None:
         super().__init__()
         assert config.n_head == config.kv_heads, "Q and KV heads must be equal; GQA not implemented yet."
+        self.config = config
         self.head_dim = config.head_dim
         qkv_heads: int = config.n_head + config.kv_heads * 2
         self.scale = self.head_dim**-0.5 if config.scale_qk else 1.0
         self.dropout = config.dropout
-        self.score_mod = self.make_self_attn_score_mod(
-            emb_weight=emb_weight,
-            num_buckets=config.relative_attention_num_buckets,
-            max_distance=config.relative_attention_max_distance,
-        )
+        # we don't have pos emb weights at the time of construction, so are not ready to bind the score_mod
+        self.score_mod = None
         self.qkv_proj = Linear(
             in_features=config.hidden_dim,
             out_features=config.head_dim * qkv_heads,
@@ -125,23 +128,12 @@ class T5EncoderSelfAttentionFlex(nn.Module):
         self.config = config
 
     @staticmethod
-    def make_self_attn_score_mod(
-        emb_weight: FloatTensor,
-        num_buckets=32,
-        max_distance=128,
-    ) -> ScoreMod:
-        # encoder self-attn is bidirectional; each direction uses half the buckets
-        half_buckets = num_buckets // 2
-
-        max_half_bucket_ix = half_buckets - 1
-
-        # (in each direction)
-        # half of the buckets are allocated for exact increments in position
-        # the other half are logarithmically growing bins in positions up to max_distance
-        max_exact = half_buckets // 2
-
-        relpos_coeff: float = (half_buckets - max_exact) / math.log(max_distance / max_exact)
-
+    def make_score_mod(pos_bias: FloatTensor) -> ScoreMod:
+        """
+        Uses basically none of the functionality of flex attention but outperforms SDPA cuDNN/cutlassF
+        and is also the fastest flex attention score_mod I was able to concoct
+        https://github.com/pytorch/pytorch/issues/138493
+        """
         def score_mod(
             score: FloatTensor,
             b: IntTensor,
@@ -149,58 +141,42 @@ class T5EncoderSelfAttentionFlex(nn.Module):
             q_idx: IntTensor,
             kv_idx: IntTensor,
         ) -> FloatTensor:
-            mem_pos = kv_idx
-            # NOTE: in decoder self-attn, if kv-cached decoding is used: we would see
-            # only the final query element, and its position would need adjusting.
-            # q_idx would not run from 0 to q_len-1,
-            # the only query would be q_idx=0, and you'd want to add q_len-1 to it, e.g.
-            #     ctx_pos = q_idx + q_len-1
-            # or just:
-            #     ctx_pos = q_len-1
-            ctx_pos = q_idx
-            relative_position = (mem_pos - ctx_pos).abs_()
-            is_small = relative_position < max_exact
-
-            # NOTE: in decoder self-attn, if kv-cached decoding is used: we would need
-            # to move the diagonal of this upper-triangular mask, so that no key gains
-            # a positive position (they'd all be behind the query).
-            # maybe like this:
-            #     excess_keys: int = k_len - q_len
-            #     relative_buckets = torch.where(mem_pos > ctx_pos + excess_keys, half_buckets, 0)
-            # errr or maybe just:
-            #     relative_buckets = 0
-            relative_buckets = torch.where(mem_pos > ctx_pos, half_buckets, 0)
-
-            relpos_distant = (max_exact + relative_position.to(torch.float32, copy=True).div_(max_exact).log_().mul_(relpos_coeff).long()).clamp_max_(max_half_bucket_ix)
-
-            relative_buckets.add_(torch.where(is_small, relative_position, relpos_distant))
-
-            return score + emb_weight[relative_buckets, h]
-
+            return score + pos_bias[h, q_idx, kv_idx]
         return score_mod
+
+    @staticmethod
+    def make_mask_mod(mask: BoolTensor) -> MaskMod:
+        def mask_mod(
+            batch: IntTensor,
+            head: IntTensor,
+            q_idx: IntTensor,
+            kv_idx: IntTensor,
+        ) -> BoolTensor:
+            return mask[batch, kv_idx] & mask[batch, q_idx]
+        return mask_mod
 
     def forward(
         self,
         x: FloatTensor,
-        mask: Optional[BoolTensor] = None,
+        block_mask: Optional[BlockMask] = None,
     ) -> FloatTensor:
+        # we refrain from binding score_mod on-demand because torch.compile would probably force you to do so repeatedly
+        assert self.score_mod is not None, "score_mod has not yet been bound. Use T5EncoderStack#bind_score_mods after loading weights."
         qkv: FloatTensor = self.qkv_proj(x)
         q, k, v = rearrange(
             qkv, "batch seq (proj heads head_dim) -> proj batch heads seq head_dim", proj=3, head_dim=self.head_dim
         ).unbind()
-        position_bias = position_bias.type_as(q)
-        # TODO: if training, then learn scales for Q and K as a proxy for learning rate
-        if mask is not None:
-            assert mask.ndim == 4, "Expected [batch, heads, q, k] attention mask"
-            position_bias = position_bias.where(mask, -1e5)
-        a = scaled_dot_product_attention(
+        # NOTE: be sure to compile T5EncoderSelfAttentionFlex#forward! if you don't, then at least compile
+        # this flex_attention operation (you could replace it with a call to .flex_utils.get_compiled_flex)
+        from torch.nn.attention.flex_attention import flex_attention
+        a = flex_attention(
             q,
             k,
             v,
-            # fused kernel requires last dimension of input to have stride 1.
-            attn_mask=position_bias.contiguous(),
-            dropout_p=self.dropout if self.training else 0.0,
+            score_mod=self.score_mod,
+            block_mask=block_mask,
             scale=self.scale,
+            flex_kernel_options=self.config.flex_kernel_options,
         )
         a = rearrange(a, "batch heads seq head_dim -> batch seq (heads head_dim)")
         o = self.o_proj(a)
@@ -218,6 +194,12 @@ class EncoderSelfAttentionFactory(Protocol):
 #### T5 encoder layers
 ####
 
+class SDPAArgs(NamedTuple):
+    position_bias: FloatTensor
+    mask: Optional[BoolTensor]
+
+class FlexArgs(NamedTuple):
+    block_mask: Optional[BlockMask]
 
 class T5EncoderLayer(nn.Module):
     attn: T5EncoderSelfAttention | T5EncoderSelfAttentionFlex
@@ -253,14 +235,13 @@ class T5EncoderLayer(nn.Module):
     def forward(
         self,
         x_r: ActAndResidual,
-        position_bias: FloatTensor,
-        attn_mask: Optional[BoolTensor] = None,
+        attn_args: SDPAArgs | FlexArgs,
     ) -> ActAndResidual:
         x, residual = x_r
         x, residual = self.ln1(x, residual=residual)
         if self.ln1_residual_scale is not None:
             residual = residual * self.ln1_residual_scale
-        x = self.attn(x, position_bias=position_bias, mask=attn_mask)
+        x = self.attn(x, *attn_args)
         x, residual = self.ln2(self.dropout(x), residual=residual)
         if self.ln2_residual_scale is not None:
             residual = residual * self.ln2_residual_scale
@@ -320,41 +301,84 @@ class T5EncoderStack(nn.Module):
         self,
         input_ids: LongTensor,
         input_mask: Optional[BoolTensor] = None,
+        # SDPA: position_bias can be re-used across invocations; best computed outside
+        position_bias: Optional[FloatTensor] = None,
+        # Flex: block_mask creation seems to dislike being inside torch.compile, so I'ma ask you to pass it in
+        block_mask: Optional[BlockMask] = None,
     ) -> FloatTensor:
         seq_len: int = input_ids.size(-1)
         input_embeds = self.vocab_embed(input_ids.flatten(end_dim=-2))
-        position_bias: FloatTensor = self.relative_attention_bias(seq_len)
-        attn_mask = None
-        if input_mask is not None:
-            match input_mask.ndim:
-                case 2:
-                    assert (
-                        input_mask.shape == input_ids.shape
-                    ), f"attn mask was 2-dim, so expected padding mask: (batch, seq_len). got {input_mask.shape} mask for {input_ids.shape} inputs."
-                    # broadcast the key-mask over all heads and queries (b h q k)
-                    attn_mask = rearrange(input_mask, "b k -> b 1 1 k")
-                case 3:
-                    assert input_mask.shape == torch.Size(
-                        [*input_ids.shape, input_ids.shape[-1]]
-                    ), f"attn mask was 3-dim, so expected packing mask: (batch, seq_len, seq_len). got {input_mask.shape} mask for {input_ids.shape} inputs."
-                    # broadcast over all heads (b h q k)
-                    attn_mask = rearrange(input_mask, "b q k -> b 1 q k")
-                case _:
-                    raise ValueError(
-                        f"Expected 2 or 3-dim input mask, got mask {input_mask.shape} for {input_ids.shape} inputs."
-                    )
+        
+        attn_args: list[SDPAArgs|FlexArgs]
+        match self.config.attn_impl:
+            case T5AttnImpl.SDPA:
+                if position_bias is None:
+                    seq_len: int = input_ids.size(-1)
+                    position_bias: FloatTensor = self.relative_attention_bias(seq_len)
 
-        # UMT5 has a position embedding per layer, and we computed all of them simultaneously
-        # note: [t]*n does not duplicate tensors, it just makes a list of n references to the same tensor
-        biases: list[FloatTensor] = position_bias.unbind() if self.config.pos_emb_per_layer else [position_bias]*self.config.num_layers
+                # UMT5 has a position embedding per layer, and we computed all of them simultaneously
+                # note: [t]*n does not duplicate tensors, it just makes a list of n references to the same tensor
+                biases: list[FloatTensor] = position_bias.unbind() if self.config.pos_emb_per_layer else [position_bias]*self.config.num_layers
+
+                attn_mask: Optional[BoolTensor] = None if input_mask is None else self.broadcast_mask(input_mask, input_ids)
+                    
+                attn_args = [SDPAArgs(
+                    position_bias=bias,
+                    mask=attn_mask,
+                ) for bias in biases]
+            case T5AttnImpl.Flex:
+                attn_args = [FlexArgs(block_mask=block_mask)]*self.config.num_layers
 
         x_r = ActAndResidual(self.dropout(input_embeds), None)
-        for layer, bias in zip(self.layers, biases):
-            assert isinstance(layer, T5EncoderLayer)
-            x_r = layer(x_r, position_bias=bias, attn_mask=attn_mask)
+        for layer, attn_args_ in zip(self.layers, attn_args):
+            layer: T5EncoderLayer
+            x_r = layer(x_r, attn_args_)
         x, residual = x_r
         x = self.ln(x, residual=residual, prenorm=False)
         return x
+
+    def bind_score_mods(self, seq_len=512) -> None:
+        """
+        [Only relevant if you're using flex attention]
+        Run this function after you have loaded weights and transferred the model to device.
+        You will probably want to run this within an inference_mode() or no_grad() context!
+        """
+        assert self.config.attn_impl == T5AttnImpl.Flex, "Only Flex attention has score_mods"
+        position_bias: FloatTensor = self.relative_attention_bias(seq_len)
+        biases: list[FloatTensor] = position_bias.unbind() if self.config.pos_emb_per_layer else [position_bias.squeeze(0)]*self.config.num_layers
+        for layer, bias in zip(self.layers, biases):
+            layer: T5EncoderLayer
+            attn: T5EncoderSelfAttentionFlex = layer.attn
+            attn.score_mod = attn.make_score_mod(bias)
+    
+    def broadcast_mask(self, input_mask: BoolTensor, input_ids: LongTensor) -> BoolTensor:
+        seq_len: int = input_ids.size(-1)
+        scores_shape = torch.Size((input_ids.size(0), self.config.n_head, seq_len, seq_len))
+        match input_mask.ndim:
+            case 2:
+                assert (
+                    input_mask.shape == input_ids.shape
+                ), f"attn mask was 2-dim, so expected padding mask: (batch, seq_len). got {input_mask.shape} mask for {input_ids.shape} inputs."
+                # broadcast the key-mask over all heads and queries (b h q k)
+                attn_mask = rearrange(input_mask, "b k -> b 1 1 k")
+            case 3:
+                assert input_mask.shape == torch.Size(
+                    [*input_ids.shape, input_ids.shape[-1]]
+                ), f"attn mask was 3-dim, so expected packing mask: (batch, seq_len, seq_len). got {input_mask.shape} mask for {input_ids.shape} inputs."
+                # broadcast over all heads (b h q k)
+                attn_mask = rearrange(input_mask, "b q k -> b 1 q k")
+            case 4:
+                try:
+                    torch.broadcast_shapes(input_mask.shape, scores_shape)
+                except RuntimeError:
+                    raise ValueError(
+                        f"input_mask ({input_mask.shape}) was not broadcastable to scores, (batch, heads, seq_len, seq_len) ({scores_shape})"
+                    )
+            case _:
+                raise ValueError(
+                    f"Expected to broadcast 2~4-dim input mask onto {scores_shape} scores, got mask {input_mask.shape} for {input_ids.shape} inputs."
+                )
+        return attn_mask
 
     def flop_count_per_sequence(self, input_ids_len: int, labels_len: int) -> int:
         # note: encoder doesn't look at labels
