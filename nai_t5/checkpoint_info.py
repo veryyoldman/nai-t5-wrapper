b1d87b497658fc069757789483299c54c47adbd4
veryyoldman <6755083+veryyoldman@users.noreply.github.com>
2025-03-09 19:55:19 +0000

    update XL scales

--- nai_t5/checkpoint_info.py
+++ nai_t5/checkpoint_info.py
@@ -12,7 +12,7 @@ enc_ffn_out_scale_dict: dict[Checkpoint, Optional[list[float]]] = {
     # 8 layers
     Checkpoint.T5v1_1Small: [*[1]*2, 1/2, *[1]*4, 1/2],
     # 24 layers
-    Checkpoint.T5v1_1XL: [*[1]*5, 1/8, *[1]*18],
+    Checkpoint.T5v1_1XL: [1, 1/2, *[1]*3, 1/4, *[1]*18],
     # 24 layers
     Checkpoint.T5v1_1XXL: [*[1]*7, 1/4, *[1]*16],
 }
